02eb186bb51249630c5b7edcb81c4b6fc333694f
morethanmini <dltkd2786@gmail.com>
2021-12-14 22:39:38 +0900

    new commit

---
+++ basic_test_6001_6008.py
@@ -0,0 +1,28 @@
+# Code up Python basic test 100
+# [기초-출력] 6001 ~ 6008
+
+#6001
+print("Hello")
+
+#6002
+print("Hello World")
+
+#6003
+print("Hello\nWorld")
+
+#6004
+print("\'Hello\'")
+
+#6005
+print("\"Hello World\"")
+
+#6006
+print("\"!@#$%^&*()\'")
+
+#6007
+print("\"C:\Download\\'hello\'.py\"")
+
+#6008
+print("print(\"Hello\\nWorld\")")
+
+
