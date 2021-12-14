cd9f7e691829317a15ac55a2d6898b794952d1bb
morethanmini <dltkd2786@gmail.com>
2021-12-14 22:51:45 +0900

    new commit

--- basic_test_6009_6024.py
+++ basic_test_6009_6024.py
@@ -1,6 +1,25 @@
 # Code up Python basic test 100
 # [기초-입출력] 6009 ~ 6024
 
+
+"""
 #6009
 a = input()
 print(a)
+
+#6010
+n = input()
+n = int(n)
+print(n)
+
+#6011
+f = input()
+f = float(f)
+print(f)
+"""
+
+#6012
+a = input()
+b = input()
+print(a)
+print(b)
