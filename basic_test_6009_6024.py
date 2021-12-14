0d195a7e59f627064641c1cf94039d2c3f9f9734
morethanmini <dltkd2786@gmail.com>
2021-12-15 00:07:24 +0900

    new commit

--- basic_test_6009_6024.py
+++ basic_test_6009_6024.py
@@ -1,7 +1,6 @@
 # Code up Python basic test 100
 # [기초-입출력] 6009 ~ 6024
 
-
 """
 #6009
 a = input()
@@ -16,10 +15,70 @@ print(n)
 f = input()
 f = float(f)
 print(f)
-"""
 
 #6012
 a = input()
 b = input()
 print(a)
 print(b)
+
+#6013
+a = input()
+b = input()
+print(b)
+print(a)
+
+#6014
+a = input()
+a = float(a)
+print(a)
+print(a)
+print(a)
+
+#6015
+a, b = input().split()
+print(a)
+print(b)
+
+#6016
+a, b = input().split()
+print(b)
+print(a)
+
+#6017
+a = input()
+print(a, a, a)
+
+#6018
+a, b = input().split(':')
+print(a, b, sep=':')
+
+#6019
+y, m, d = input().split('.')
+print(d, m, y, sep='-')
+
+#6020
+ymd, s = input().split('-')
+print(ymd+s)
+
+#6021
+s = input()
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+
+#6022
+s = input()
+print(s[0:2], s[2:4], s[4:6])
+
+#6023
+h, m, s= input().split(':')
+print(m)
+
+#6024
+w1, w2 = input().split()
+s = w1 + w2
+print(s)
+"""
