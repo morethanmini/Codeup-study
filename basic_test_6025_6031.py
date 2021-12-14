6f322b8a4fd59d50c875a8c47a2ee84cfc34056e
morethanmini <dltkd2786@gmail.com>
2021-12-15 00:38:33 +0900

    new commit

--- basic_test_6025_6031.py
+++ basic_test_6025_6031.py
@@ -1,7 +1,37 @@
 # Code up Python basic test 100
 # [기초-입출력] 6025 ~ 6031
 
+"""
 #6025
 a, b = input().split()
 c = int(a) + int(b)
 print(c)
+
+#6026
+a = input()
+b = input()
+print(float(a) + float(b))
+
+#6027 출력변환
+a = input()
+n = int(a)
+print('%x'%n)
+
+#6028 출력변환
+a = input()
+n = int(a)
+print('%X'%n)
+
+#6029
+a = input()
+n = int(a, 16)
+print("%o"%n)
+
+#6030
+a = ord(input())
+print(a)
+
+#6031
+a = int(input())
+print(chr(a))
+"""
