7a1aec2512cbb21fedf9b578430c77f00a2e81a8
morethanmini <dltkd2786@gmail.com>
2021-12-15 13:39:23 +0900

    new commit

--- basic_test_6032_6045.py
+++ basic_test_6032_6045.py
@@ -1,5 +1,83 @@
 # Code up Python basic test 100
 # [기초-산술연산] 6025 ~ 6031
 
+"""
 #6032
-print("Hello")
+a = input()
+a = int(a)
+print(-(a))
+
+#6033
+a = ord(input())
+print(chr(a+1))
+
+#6034
+a, b = input().split()
+c = int(a) - int(b)
+print(c)
+
+#6035
+f1, f2 = input().split()
+m = float(f1) * float(f2)
+print(m)
+
+#6036
+w, n = input().split()
+print(w*int(n))
+
+#6037
+n = input()
+s = input()
+print(int(n)*s)
+
+#6038
+a, b = input().split()
+c = int(a)**int(b)
+print(c)
+
+#6039
+f1, f2 = input().split()
+c = float(f1)**float(f2)
+print(c)
+
+#6040
+a, b = input().split()
+print(int(a)//int(b))
+
+#6041
+a, b = input().split()
+c = int(a)%int(b)
+print(c)
+
+#6042
+a=float(input())
+print( format(a, ".2f") )
+
+#6043
+f1, f2 = input().split()
+c = float(f1)/float(f2)
+print(format(c, ".3f"))
+
+#6044
+a, b = input().split()
+a, b =int(a), int(b)
+sum = a+b
+sub = a-b
+mul = a*b
+x = a//b
+d = a%b
+z = a/b
+print(sum)
+print(sub)
+print(mul)
+print(x)
+print(d)
+print(format(z, ".2f"))
+
+#6045
+a, b, c = input().split()
+a, b, c = int(a), int(b), int(c)
+d = a+b+c
+e = (a+b+c)/3
+print(d, format(e, ".2f"))
+"""
