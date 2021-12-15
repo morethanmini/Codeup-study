dd59878115880d4796aaa82c250808a9fc65aca7
morethanmini <dltkd2786@gmail.com>
2021-12-15 21:02:50 +0900

    new commit

---
+++ basic_test_6065_6076.py
@@ -0,0 +1,16 @@
+# Code up Python basic test 100
+# [기초-실행구조] 6065 ~ 6076
+
+#6065
+a, b, c = map(int, input().split())
+def evennumber():
+    if a%2==0 :
+        print(a)
+
+    if b%2==0 :
+        print(b)
+
+    if c%2==0 :
+        print(c)
+
+evennumber()
