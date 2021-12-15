0b9946b433c650628fe8a52270ce5a7667f1fc4b
morethanmini <dltkd2786@gmail.com>
2021-12-15 22:27:39 +0900

    new commit

--- basic_test_6077_6091.py
+++ basic_test_6077_6091.py
@@ -1,4 +1,18 @@
 # Code up Python basic test 100
 # [기초-실행구조] 6077 ~ 6091
 
+"""
 #6077
+a = int(input())
+b = 0
+for i in range(1, a+1):
+    if i%2==0:
+        b += i
+print(b)
+"""
+a = int(input())
+b = 0
+for i in range(1, a+1):
+    if i%2==1:
+        b+=i
+        print(b)
