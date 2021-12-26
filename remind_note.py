6968c50b3f0dbba11b4b6fd3b2505cdb1d76a6a7
morethanmini <dltkd2786@gmail.com>
2021-12-26 18:24:54 +0900

    new commit

--- remind_note.py
+++ remind_note.py
@@ -1,7 +1,12 @@
 #remind
-
-n = 1
+"""
+n = 1 #처음 조건 검사를 통과하기 위해 0이 아닌 값을 임의로 저장
 while n!=0:
     n = int(input())
     if n != 0:
         print(n)
+"""
+
+n = int(input())
+for i in range(0, n+1):
+    print(i)
